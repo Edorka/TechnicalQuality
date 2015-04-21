f0d8f1b35c58493126c60e25803973b1cb0a5a9f
Eduardo Orive <edorka@gmail.com>
2015-04-21 11:02:44 +0000

    sketch of scoring

--- app/scripts/controllers/execution.js
+++ app/scripts/controllers/execution.js
@@ -8,10 +8,14 @@
  * Controller of the technicalQualityApp
  */
 angular.module('technicalQualityApp')
-  .controller('ExecutionCtrl', function ($scope, $routeParams, ruleSet, competitors) {
+  .controller('ExecutionCtrl', function ($scope, $location, $routeParams, ruleSet, competitors) {
         var cathegorie = $routeParams.cathegorie;
         var competitorId = $routeParams.competitorId;
         $scope.cathegorieDescription = ruleSet.getCathegorie(cathegorie);
         $scope.competitor = competitors.get(competitorId);
         $scope.cathegorieDescription = ruleSet.getCathegorie(cathegorie);
+        $scope.commit = function commitAndReturn(){
+            $scope.competitor.score = 5.00;
+            $location.path("/");
+        }
   });
